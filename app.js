5038dfb01b92f9453582b1f70122ceb44f78a11b
atreat738 <atreat738@gmail.com>
2021-12-09 08:15:43 -0600

    Seperated ship array from length

--- app.js
+++ app.js
@@ -1,24 +1,36 @@
+//Types of ships: Carrier(5), Battleship(4), Cruiser(3), Submarine(3), Destroyer(2)
+
 function Ship(length) {
     
-    function findlength() {
-        let shipLength = [];
+    function createShipArray() {
+        let shipArray = [];
         for(i = 0; i < length; i++) {
             //shiplength will be an array which by default marks every spot in the array as safe(not hit) 
-            shipLength.push('safe');
+            shipArray.push('safe');
         }
-         return shipLength;
+         return shipArray;
     }
 
-    
+    // function hit(index) {
+    //     let hitPosition = 
+    // }
+
+
+
+
     return {
-        length: findlength(length),
+        length: length,
+        shipArray: createShipArray(),
         hit: function(num) {
             //input number, and mark that position as hit
         },
         isSunk: function() {
             //Check to see if the ship is sunk or not.
         },
+
     }
 
 }
+
+
 module.exports = Ship;
--- app.test.js
+++ app.test.js
@@ -1,11 +1,15 @@
+const { test, expect } = require('@jest/globals');
 const Ship = require('./app');
 
 //ship needs to be an object that:
 //1.Has a length.
 
-test('What is the ship length?', () => {
-    expect(Ship(4).length).toEqual(['safe', 'safe', 'safe', 'safe'])
-} );
+test('Did the ship array get built properly', () => {
+    expect(Ship(4).shipArray).toEqual(['safe', 'safe', 'safe', 'safe'])
+});
 
 //2.Has either been hit or not been hit
+test('Does the ship have a valid length', () => {
+    expect(Ship(4).length).toBe(4)
+});
 //3.Has either been sunk or not sunk
