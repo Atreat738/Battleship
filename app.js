7786e5d318cb5e46f4106ebae1c3f5d77d2b6985
atreat738 <atreat738@gmail.com>
2021-12-08 18:38:36 -0600

    Created and passed first test

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 function Ship(length) {
+    
+    function findlength() {
+        let shipLength = [];
+        for(i = 0; i < length; i++) {
+            //shiplength will be an array which by default marks every spot in the array as safe(not hit) 
+            shipLength.push('safe');
+        }
+         return shipLength;
+    }
+
+    
     return {
-        length,
+        length: findlength(length),
         hit: function(num) {
             //input number, and mark that position as hit
         },
--- app.test.js
+++ app.test.js
@@ -2,6 +2,10 @@ const Ship = require('./app');
 
 //ship needs to be an object that:
 //1.Has a length.
+
+test('What is the ship length?', () => {
+    expect(Ship(4).length).toEqual(['safe', 'safe', 'safe', 'safe'])
+} );
+
 //2.Has either been hit or not been hit
 //3.Has either been sunk or not sunk
-
